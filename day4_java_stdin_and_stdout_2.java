8b7767818bc3a069fee9495362a116faa25db339
Aco Deswan <99626768+AcoDeswanYasri@users.noreply.github.com>
2022-02-22 19:36:36 +0700

    Add files via upload

---
+++ day4_java_stdin_and_stdout_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double c = sc.nextDouble();
+        sc.nextLine();
+        String o = sc.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + o);
+        System.out.println("Double: " + c);
+        System.out.println("Int: " + a);
+    }
